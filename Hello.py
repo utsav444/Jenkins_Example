057d2e28b12e40830a23dd57791b2c5714d188aa
Utsav Sangani <124497683+utsav444@users.noreply.github.com>
2025-04-15 13:26:24 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -6,7 +6,7 @@ def main():
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
     
     print("\U0001f527 Jenkins CI/CD Demo Running...")
-    print("This is a simple Jenkins pipeline demo!")
+    print("This is a very simple Jenkins pipeline demo!")
     print("All systems go!")
 
 if __name__ == "__main__":
